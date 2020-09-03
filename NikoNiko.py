9ac349a05abb138f5dae64b5933cc95640e1c52a
Aymeric Deschard <aymeric.deschard@disroot.org>
2020-09-03 08:43:47 +0200

    remove $ before var names

---
+++ NikoNiko.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import os
+
+MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+if __name__ == "__main__":
+	from src import Mood, Config, Illuca
+	CONFIG = Config(MODULE_PATH)
+	conn = Illuca(CONFIG.endpoint, os.path.join(MODULE_PATH, "failures_pages"))
+	conn.connect(CONFIG.username, CONFIG.password)
+	pick = Mood.random_pick()
+	print(f"Mood selected: {pick}")
+	pst = conn.post_mood(pick)
+	print("Is post successful: {pst}")
