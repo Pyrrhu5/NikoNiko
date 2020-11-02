4faf73cba6b105e50351ad4352cefd3f78211fad
Aymeric Deschard <aymeric.deschard@disroot.org>
2020-11-02 13:28:59 +0100

    Comments for Main

--- NikoNiko.py
+++ NikoNiko.py
@@ -16,8 +16,11 @@ if __name__ == "__main__":
 	print(f"{' ' * ((80 - len(today))//2)}{today}")
 	print("="*80)
 
+	# Connects to Illuca server
 	conn = Illuca(CONFIG.endpoint, os.path.join(MODULE_PATH, "failures_pages"))
 	conn.connect(CONFIG.username, CONFIG.password)
+
+	# Post a mood
 	pick = Mood.random_pick()
 	print(f"Mood selected: {pick}")
 	pst = conn.post_mood(pick)
