0aaff9c7b350cee2b93793c69b8827c2fae7653a
Aymeric Deschard <aymeric.deschard@disroot.org>
2020-11-02 13:28:28 +0100

    Added date to stdout for logs

--- NikoNiko.py
+++ NikoNiko.py
@@ -2,12 +2,20 @@
 # -*- coding: UTF-8 -*-
 
 import os
+from datetime import datetime
 
 MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == "__main__":
 	from src import Mood, Config, Illuca
 	CONFIG = Config(MODULE_PATH)
+
+	# Print current date for logs
+	print("="*80)
+	today = datetime.today().strftime("%d/%m/%y")
+	print(f"{' ' * ((80 - len(today))//2)}{today}")
+	print("="*80)
+
 	conn = Illuca(CONFIG.endpoint, os.path.join(MODULE_PATH, "failures_pages"))
 	conn.connect(CONFIG.username, CONFIG.password)
 	pick = Mood.random_pick()
